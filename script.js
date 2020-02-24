a21607b5356c4bc99dbdeca409576ecaf2464223
unknown <George>
2020-02-24 17:32:02 +0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,138 @@
+// console.log("Hello world");
+
+// var person = "John";
+// console.log(person);
+
+// person = "Nick";
+// console.log(person);
+
+// var first-Name = "Bob";
+
+// var LastName = "Doe";
+
+// let age = 30;
+
+// console.log(age);
+
+// age = 35;
+
+// console.log(age);
+
+// const city = "London";
+
+// console.log(city);
+
+// const str = "John is a developer";
+// console.log(str);
+// console.log(typeof str);
+
+// const num = "27.23";
+// console.log(num);
+// console.log(typeof num);
+
+// const num1 = 10;
+// const num2 = 20;
+// const bool = true;
+// console.log(typeof bool);
+
+// let a;
+// console.log(a);
+// console.log(typeof a);
+
+// const b = null;
+// console.log(b);
+
+// const a = 5 / 5;
+// console.log(a);
+
+// const b = (5 + 5) * 5;
+// console.log(b);
+
+// const num1 = 10;
+// const num2 = "10";
+
+// const a = 10 * "20";
+// console.log(a);
+
+// const b = "Hello " + "there";
+// console.log(b);
+
+// const c = "a" + 10 + 15;
+// console.log(c);
+
+// const kid = "Alexis";
+// const gender = "Female";
+
+// if (gender == "Male") {
+//   console.log(kid + " is my son");
+// } else {
+//   console.log(kid + " is my daughter");
+// }
+
+// const prof = "engineer";
+
+// if (prof === "composer") {
+//   console.log(prof + " teaches students");
+// } else if (prof === "composer") {
+//   console.log(prof + " creates music");
+// } else {
+//   console.log("Professions do not match");
+// }
+
+// if (!5 === 5) {
+//   console.log("Condition is true");
+// } else {
+//   console.log("Condition is false");
+// }
+
+// function test() {
+//   console.log("Hello there");
+// }
+
+// test();
+
+// function passExam(name, score) {
+//   const passUni = 71;
+//   const passColl = 51;
+
+//   if (score >= passUni) {
+//     console.log(name + " enrolled at the University with " + score + " points");
+//   } else if (score >= passColl) {
+//     console.log(name + " enrolled at the college with " + score + " points");
+//   } else {
+//     console.log(name + " failed");
+//   }
+// }
+
+// function calcScore(quizScore, essayScore) {
+//   const score = quizScore + essayScore;
+
+//   return score;
+// }
+
+// passExam("Nick", 85);
+// passExam("Mary", 75);
+// passExam("John", calcScore(29, 37));
+
+// const multiply = () => 10 * 10;
+
+// console.log(multiply());
+
+// const arr = ["Ann", "Bob", "John", 10, true, [1, 2, 3]];
+// console.log(arr);
+// console.log(arr[5][1]);
+
+// const colors = ["white", "black", "red"];
+// colors[1] = "green";
+
+// colors.push("blue");
+
+// colors.pop();
+
+// colors.shift();
+
+// colors.unshift("purple");
+
+// console.log(colors);
+
+// console.log(colors.indexOf("yellow"));
